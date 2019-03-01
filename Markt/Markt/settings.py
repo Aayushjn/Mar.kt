55deeaf38a13a4c08b7c9163f79a73832d66f68c
Aayushjn <aayushjn11@gmail.com>
2019-03-01 10:27:11 +0530

    Changed DB configuration to MySQL

--- Markt/Markt/settings.py
+++ Markt/Markt/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'Markt.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/etc/mysql/markt.cnf'
+        },
     }
 }
 
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
