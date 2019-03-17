e92c95bf357b33bf03afb3bca65977657c0849a7
Aayushjn <aayushjn11@gmail.com>
2019-03-17 11:04:21 +0530

    Added models for each app

--- Markt/seller/models.py
+++ Markt/seller/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from Markt.homepage.models import User
+
+
+class Bid(models.Model):
+    id = models.IntegerField(max_length=10)
+    product_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    buyer_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    price = models.DecimalField()
+    timestamp = models.DateTimeField()
+
+
+class Mail(models.Model):
+    id = models.IntegerField(max_length=10)
+    buyer_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    vendor_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    bid_id = models.ForeignKey(Bid, on_delete=models.CASCADE)
+    message_type = models.IntegerField(max_length=5)
