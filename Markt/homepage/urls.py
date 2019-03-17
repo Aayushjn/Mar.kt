fb93709b5209aa8c4ac5432c7878cf4c4939afe0
Aayushjn <aayushjn11@gmail.com>
2019-03-17 17:59:41 +0530

    Added URL redirection

--- Markt/homepage/urls.py
+++ Markt/homepage/urls.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
-app_name='homepage'
+app_name = 'homepage'
 
 urlpatterns = [
-    path('',views.display_home,name="home"),
-    path('login/',views.display_login,name="login"),
-    path('signup/',views.display_signup,name="signup")
-     
+    path('', views.display_home, name="home"),
+    path('login/', views.display_login, name="login"),
+    path('signup/', views.display_signup, name="signup")
+
 ]
--- Markt/homepage/views.py
+++ Markt/homepage/views.py
@@ -1,31 +1,32 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from urllib import request
 
-# Create your views here.
 
 def display_home(request):
     # use request.session to pass anything you need between views
     # delete sessions on pages that aren't dashboard pages to force logout  
-    
-    #request.session.delete()
-    
+
+    # request.session.delete()
+
     # used to display variables within frontend
-    context={
-        'page_name':"Home",
+    context = {
+        'page_name': "Home",
     }
 
-    return render(request,'homepage/index.html',context)
+    return render(request, 'homepage/index.html', context)
+
 
 def display_login(request):
-    context={
-        'page_name':"Login"
+    context = {
+        'page_name': "Login"
     }
 
-    return render(request,'homepage/login.html',context)
+    return render(request, 'homepage/login.html', context)
+
 
 def display_signup(request):
-    context={
-        'page_name':"Sign Up"
+    context = {
+        'page_name': "Sign Up"
     }
 
-    return render(request,'homepage/registration.html',context)
+    return render(request, 'homepage/registration.html', context)
