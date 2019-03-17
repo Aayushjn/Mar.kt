c3c84ecd2f8f279f8b1c4a047d735522eecf1388
Aayushjn <aayushjn11@gmail.com>
2019-03-17 11:34:30 +0530

    Updated model fields

--- Markt/buyer/models.py
+++ Markt/buyer/models.py
@@ -4,11 +4,11 @@ from Markt.homepage.models import User
 
 
 class Product(models.Model):
-    id = models.IntegerField(max_length=10)
+    id = models.IntegerField(max_length=10, primary_key=True)
     name = models.CharField(max_length=50)
     category = models.CharField(max_length=30)
     vendor_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    is_sold = models.BinaryField()
+    is_sold = models.BooleanField(default=False)
     description = models.CharField(max_length=300)
     minimum_bid = models.FloatField()
     current_high_bid = models.FloatField()
--- Markt/seller/models.py
+++ Markt/seller/models.py
@@ -4,16 +4,16 @@ from Markt.homepage.models import User
 
 
 class Bid(models.Model):
-    id = models.IntegerField(max_length=10)
+    id = models.IntegerField(max_length=10, primary_key=True)
     product_id = models.ForeignKey(User, on_delete=models.CASCADE)
     buyer_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    price = models.DecimalField()
-    timestamp = models.DateTimeField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now=True)
 
 
 class Mail(models.Model):
-    id = models.IntegerField(max_length=10)
+    id = models.IntegerField(max_length=10, primary_key=True)
     buyer_id = models.ForeignKey(User, on_delete=models.CASCADE)
     vendor_id = models.ForeignKey(User, on_delete=models.CASCADE)
     bid_id = models.ForeignKey(Bid, on_delete=models.CASCADE)
-    message_type = models.IntegerField(max_length=5)
+    message_type = models.PositiveSmallIntegerField()
