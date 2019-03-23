ee1c99b39a5cafea6cb449d277692d334334076f
Aayushjn <aayushjn11@gmail.com>
2019-03-23 08:37:21 +0530

    Updated model to use explicit primary key

--- Markt/homepage/models.py
+++ Markt/homepage/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class User(models.Model):
-    id = models.IntegerField()
+    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=50)
     address = models.CharField(max_length=100)
     email_id = models.EmailField(primary_key=True)
