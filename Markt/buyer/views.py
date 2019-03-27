5927561c1a6a382b2f606891bf84d5f8819986e3
Aditi <aditiraops@gmail.com>
2019-03-28 00:33:51 +0530

    added buyer category page

--- Markt/buyer/views.py
+++ Markt/buyer/views.py
@@ -13,6 +13,8 @@ def display_buyer_cat(request):
     context = {
         'page_name': "Buyer Dashboard",
         'category': "Generic Category",
+        'base_bid':100,
+        'high':300,
     }
 
     return render(request, 'buyer/buyer-category.html', context)
