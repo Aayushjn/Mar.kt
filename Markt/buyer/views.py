5584a50f52d4ee736f968e2d7da2874d9100ad61
Aayushjn <aayushjn11@gmail.com>
2019-03-27 23:16:42 +0530

    Minor refactoring

--- Markt/buyer/views.py
+++ Markt/buyer/views.py
@@ -1,33 +1,36 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def display_buyer_dash(request):
     context = {
         'page_name': "Buyer Dashboard",
-        
+
     }
 
     return render(request, 'buyer/buyer-dashboard.html', context)
+
 
 def display_buyer_cat(request):
     context = {
         'page_name': "Buyer Dashboard",
         'category': "Generic Category",
-        'base_bid':100,
-        'high':300,
+        'base_bid': 100,
+        'high': 300,
     }
 
     return render(request, 'buyer/buyer-category.html', context)
+
 
 def display_buyer_item(request):
     context = {
         'page_name': "Item Display",
         'category': "Generic Category",
         'item': "Generic Item",
-        'base_bid':100,
-        'high':300,
-        'description':"",
-        'seller':"Generic Seller",
+        'base_bid': 100,
+        'high': 300,
+        'description': "",
+        'seller': "Generic Seller",
 
     }
 
