8248e1e904700e8f329450ac0d1f32ad4473b7b2
span09 <spandanabalumuri99@gmail.com>
2019-03-28 08:37:22 +0530

    modified signup

--- Markt/homepage/models.py
+++ Markt/homepage/models.py
@@ -2,9 +2,8 @@ from django.db import models
 
 
 class User(models.Model):
-    id = models.IntegerField()
     name = models.CharField(max_length=50)
-    email_id = models.EmailField(primary_key=True)
+    email_id = models.EmailField()
     password = models.CharField(max_length=20)
     phone_number = models.CharField(max_length=10)
     share_email = models.BooleanField(default=True)
